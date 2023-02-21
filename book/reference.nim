ad92356fa5cfb48519563681e8924f95e2c6573b
Philipp Doerner <philippmdoerner@web.de>
2023-02-21 19:20:32 +0100

    mend

---
+++ book/reference.nim
@@ -0,0 +1,11 @@
+import nimib, nimibook
+
+
+nbInit(theme = useNimibook)
+
+nbText: """
+# Reference Widget Documentation
+The following provides reference documentation for various widgets provided by owlkettle.
+"""
+
+nbSave
