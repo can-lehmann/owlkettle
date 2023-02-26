e9f930593c76d1fe30642360a2bf80ceaf760337
Philipp Doerner <philippmdoerner@web.de>
2023-02-26 09:01:21 +0100

    Move contributing section to the bottom
    Originally the "Reference" section was the last section of the nimibook
    However, that was only because of a bug in nimibook that
    indented the entries incorrectly.
    That bug has recently been fixed, thus I'm reverting that change.

--- nbook.nim
+++ nbook.nim
@@ -6,26 +6,25 @@ var book = initBookWithToc:
   entry("Welcome to Owlkettle!", "README.md")
   entry("Tutorial", "docs/tutorial.md")
   entry("General Examples", "examples/README.md")
-  
+
   entry("Tooling", "docs/recommended_tools.md")
-  
+
   section("Reference", "book/reference.nim"):
     entry("General Widget Reference", "../docs/widgets.md")
     entry("Adwaita Widget Reference", "../docs/widgets_adwaita.md")
     entry("DataEntry Widget Reference", "../docs/widgets_dataentries.md")
-  
+
   #entry("Migrating", "docs/migrating_1_to_2.md")
   #entry("Cross Compiling", "docs/cross_compiling.md")
-  entry("Contributing", "CONTRIBUTING.md")
-  
+
   section("Internals", "book/internals.nim"):
     entry("Custom Widgets", "internals/custom_widgets.nim")
-    
+
     section("Adders", "internals/adders.nim"):
       entry("One Adder", "adders/one_adder.nim")
       entry("Adders With Properties", "adders/adders_with_properties.nim")
       entry("Multiple Adders", "adders/multiple_adders.nim")
-    
+
     section("Hooks", "internals/hooks.nim"):
       entry("Build Hook", "hooks/build_hook.nim")
       entry("BeforeBuild Hook", "hooks/before_build_hook.nim")
@@ -35,4 +34,5 @@ var book = initBookWithToc:
       entry("Property Hook", "hooks/property_hook.nim")
       entry("Read Hook", "hooks/read_hook.nim")
 
+  entry("Contributing", "CONTRIBUTING.md")
 nimibookCli(book)
