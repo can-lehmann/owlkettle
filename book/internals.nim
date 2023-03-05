58d4a47de23b6599eea43f4e917dbbd7a56051bf
Philipp Doerner <philippmdoerner@web.de>
2023-03-05 17:02:05 +0100

    Split text-sections with headings
    It may be beneficial so users can more quickly find what they're looking for.

--- book/internals.nim
+++ book/internals.nim
@@ -15,7 +15,8 @@ nbText: """
 nbText: """
 ## **Widget-Basics**
 
-Every widget in owlkettle is either a `renderable` or a `viewable` widget.
+### **Viewables vs. Renderables**
+Every widget in owlkettle is a child of `Widget` and either a `renderable` or a `viewable` widget.
 
 `Renderable` widgets provide declarative interfaces to GTK widgets.
 For example `Button`, `Window` and `Entry` are renderable widgets.
@@ -24,6 +25,8 @@ For example `Button`, `Window` and `Entry` are renderable widgets.
 
 `Viewable` widgets are abstractions over renderable widgets.
 Owlkettle applications and any custom widgets written by you are usually implemented as viewable widgets.
+
+### **Widget and WidgetState**
 
 Every widget consists of a `WidgetState` and a `Widget`, independent of whether it is `renderable` or `viewable`.
 The `WidgetState` represents the internal state of the widget which is persistent between redraws.
