417b6d6267dee841d855584cae3346ab832f3748
Philipp Doerner <philippmdoerner@web.de>
2023-09-16 14:52:44 +0200

    #82 Add doc comment

--- owlkettle/common.nim
+++ owlkettle/common.nim
@@ -71,6 +71,11 @@ proc newExport*(node: NimNode): NimNode =
   result = newTree(nnkPostfix, ident("*"), node)
 
 template customPragmas*() =
+  ## Definess a custom pragma called "locker".
+  ## This does nothing when compiled with a nim version 2.0 or higher,
+  ## but applies the "locks: 0" pragma if compiled for e.g. 1.6.X.
+  ## "locks: 0" is applied to ensure that this code never accesses locked data in user-defined procs.
+  ## Its main purpose is silencing compiler-warnings for nim 1.6.X.
   when NimMajor >= 2:
     {.pragma: locker.}
   else:
