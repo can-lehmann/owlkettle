f8869944d9e90fc3a99fd60784f42257f5ad8544
Philipp Doerner <philippmdoerner@web.de>
2023-09-16 15:07:02 +0200

    #82 Remove now pointless file

--- owlkettle/customPragmas.nim
+++
@@ -1,4 +0,0 @@
-## Definess a custom pragma called "locker".
-## This does nothing when compiled with a nim version 2.0 or higher,
-## but applies the "locks: 0" pragma if compiled for e.g. 1.6.X.
-## "locks: 0" is applied to ensure that this code never accesses locked data in user-defined procs.
