d5d08c94b99fcaa872bb6610cfba5ae720851e5c
Philipp Doerner <philippmdoerner@web.de>
2023-10-02 17:31:32 +0200

    #93 Update Autoform for scale

--- examples/widgets/scale.nim
+++ examples/widgets/scale.nim
@@ -23,6 +23,41 @@
 import std/[options, sequtils, json]
 import owlkettle, owlkettle/[dataentries, widgetUtils, autoform, adw]
 
+proc toFormField(state: auto, fieldName: static string, typ: typedesc[seq[ScaleMark]]): Widget =
+  return gui:
+    ExpanderRow:
+      title = fieldName
+      
+      for index, mark in state.marks:
+        ActionRow {.addRow.}:
+          title = fieldName & $index
+          
+          NumberEntry {.addSuffix.}:
+            value = mark.value
+            xAlign = 1.0
+            maxWidth = 8
+            proc changed(value: float) =
+              state.marks[index].value = value
+          
+          DropDown {.addSuffix.}:
+            items = ScalePosition.items.toSeq().mapIt($it)
+            selected = mark.position.int
+            proc select(enumIndex: int) =
+              state.marks[index].position = enumIndex.ScalePosition
+              
+          Button {.addSuffix.}:
+            icon = "user-trash-symbolic"
+            proc clicked() =
+              state.marks.delete(index)
+      
+      ListBoxRow {.addRow.}:
+        Button:
+          icon = "list-add-symbolic"
+          style = [ButtonFlat]
+          proc clicked() =
+            state.marks.add(ScaleMark())
+
+
 viewable App:
   min: float = 0
   max: float = 100
@@ -44,131 +79,7 @@ method view(app: AppState): Widget =
       defaultSize = (800, 600)
       
       Box(orient = OrientX):
-        Box(orient = OrientY) {.expand: false.}:
-          sizeRequest = (300, -1)
-          
-          HeaderBar {.expand: false.}:
-            showTitleButtons = false
-          
-          ScrolledWindow:
-            PreferencesGroup:
-              title = "Fields"
-              margin = 12
-              
-              ActionRow:
-                title = "min"
-                NumberEntry {.addSuffix.}:
-                  value = app.min
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.min = value
-              
-              ActionRow:
-                title = "max"
-                NumberEntry {.addSuffix.}:
-                  value = app.max
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.max = value
-              
-              ActionRow:
-                title = "value"
-                NumberEntry {.addSuffix.}:
-                  value = app.value
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.value = value
-              
-              ActionRow:
-                title = "inverted"
-                Switch {.addSuffix.}:
-                  state = app.inverted
-                  proc changed(state: bool) =
-                    app.inverted = state
-              
-              ActionRow:
-                title = "showValue"
-                Switch {.addSuffix.}:
-                  state = app.showValue
-                  proc changed(state: bool) =
-                    app.showValue = state
-              
-              ActionRow:
-                title = "stepSize"
-                NumberEntry {.addSuffix.}:
-                  value = app.stepSize
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.stepSize = value 
-              
-              ActionRow:
-                title = "pageSize"
-                NumberEntry {.addSuffix.}:
-                  value = app.pageSize
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.pageSize = value
-              
-              ActionRow:
-                title = "showFillLevel"
-                Switch {.addSuffix.}:
-                  state = app.showFillLevel
-                  proc changed(state: bool) =
-                    app.showFillLevel = state
-              
-              ActionRow:
-                title = "precision"
-                NumberEntry {.addSuffix.}:
-                  value = app.precision.float
-                  xAlign = 1.0
-                  maxWidth = 8
-                  proc changed(value: float) =
-                    app.precision = value.int
-              
-              ComboRow:
-                title = "orient"
-                items = mapIt(low(Orient)..high(Orient), $it)
-                selected = ord(app.orient)
-                proc select(index: int) =
-                  app.orient = Orient(index)
-              
-              ComboRow:
-                title = "valuePosition"
-                items = mapIt(low(ScalePosition)..high(ScalePosition), $it)
-                selected = ord(app.valuePosition)
-                proc select(index: int) =
-                  app.valuePosition = ScalePosition(index)
-              
-              ExpanderRow:
-                title = "marks"
-                
-                for it, mark in app.marks:
-                  ActionRow {.addRow.}:
-                    title = "Mark " & $it
-                    
-                    NumberEntry {.addSuffix.}:
-                      value = mark.value
-                      xAlign = 1.0
-                      maxWidth = 8
-                      proc changed(value: float) =
-                        app.marks[it].value = value
-                    
-                    Button {.addSuffix.}:
-                      icon = "user-trash-symbolic"
-                      proc clicked() =
-                        app.marks.delete(it)
-                
-                ListBoxRow {.addRow.}:
-                  Button:
-                    icon = "list-add-symbolic"
-                    style = [ButtonFlat]
-                    proc clicked() =
-                      app.marks.add(ScaleMark())
+        insert app.toAutoForm()
         
         Separator() {.expand: false.}
         
--- owlkettle/autoform.nim
+++ owlkettle/autoform.nim
@@ -17,68 +17,95 @@ proc `%`*(t: tuple): JsonNode =
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[SomeFloat]): Widget =
   return gui:
-    NumberEntry(value = state.getField(fieldName)):
-      proc changed(value: float) =
-        state.getField(fieldName) = value
+    ActionRow:
+      title = fieldName
+      NumberEntry() {.addSuffix.}:
+        value = state.getField(fieldName)
+        xAlign = 1.0
+        maxWidth = 8
+        proc changed(value: float) =
+          state.getField(fieldName) = value
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[SomeInteger]): Widget =
   return gui:
-    NumberEntry(value = state.getField(fieldName).float):
-      proc changed(value: float) =
-        state.getField(fieldName) = value.int
+    ActionRow:
+      title = fieldName
+      NumberEntry() {.addSuffix.}:
+        value = state.getField(fieldName).float
+        xAlign = 1.0
+        maxWidth = 8
+        proc changed(value: float) =
+          state.getField(fieldName) = value.int
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[string]): Widget =
   return gui:
-    Entry(text = state.getField(fieldName)):
-      proc changed(text: string) =
-        state.getField(fieldName) = text
+    ActionRow:
+      title = fieldName
+      Entry() {.addSuffix.}:
+        text = state.getField(fieldName)
+        proc changed(text: string) =
+          state.getField(fieldName) = text
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[bool]): Widget =
     return gui:
-      Box():
-        Switch(state = state.getField(fieldName)) {.vAlign: AlignCenter, expand: false.}:
-          proc changed(newVal: bool) =
-            state.getField(fieldName) = newVal
+      ActionRow:
+        title = fieldName
+        Box() {.addSuffix.}:
+          Switch() {.vAlign: AlignCenter, expand: false.}:
+            state = state.getField(fieldName)
+            proc changed(newVal: bool) =
+              state.getField(fieldName) = newVal
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[object | ref object | tuple | seq]): Widget =
     return gui:
-      Entry(text = $ %*state.getField(fieldName)):
-        proc changed(text: string) =
-          try:
-            state.getField(fieldName) = text.parseJson().to(typ)
-          except Exception: discard
+      ActionRow:
+        title = fieldName
+        Entry(text = $ %*state.getField(fieldName)) {.addSuffix.}:
+          proc changed(text: string) =
+            try:
+              state.getField(fieldName) = text.parseJson().to(typ)
+            except Exception: discard
 
 proc toFormField[T: enum](state: auto, fieldName: static string, typ: typedesc[T]): Widget =
   let options: seq[string] = T.items.toSeq().mapIt($it)
   return gui:
-    DropDown:
+    ComboRow:
+      title = "orient"
       items = options
-      selected = state.getField(fieldName).int
+      selected = ord(state.getField(fieldName))
       proc select(enumIndex: int) =
         state.getField(fieldName) = enumIndex.T
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[DateTime]): Widget =
   return gui:
-    Calendar:
-      date = state.getField(fieldName)
-      proc daySelected(date: DateTime) =
-        state.getField(fieldName) = date
+    ActionRow:
+      title = fieldName
+      Calendar {.addSuffix.}:
+        date = state.getField(fieldName)
+        proc daySelected(date: DateTime) =
+          state.getField(fieldName) = date
   
 proc toAutoForm*[T](app: T, ignoreFields: static seq[string]): Widget =
-  var fieldWidgets: seq[tuple[name: string, field: AlignedChild[Widget]]] = @[]
+  var fieldWidgets: seq[Widget] = @[]
   for name, value in app[].fieldPairs:
     when name notin ["app", "viewed"] and name notin ignoreFields:
       let fieldWidget = app.toFormField(name, value.type)
-      let alignedWidget = AlignedChild[Widget](widget: fieldWidget)
-      fieldWidgets.add((name, alignedWidget))
-
+      fieldWidgets.add(fieldWidget)
+      
   result = gui:
-    Box(orient = OrientY, spacing = 6, margin = 12):
-      for field in fieldWidgets:
-        ActionRow {.expand: false.}:
-          margin = 6
-          title = field.name
-          suffixes = @[field.field]
+    Box(orient = OrientY):
+      sizeRequest = (300, -1)
+      
+      HeaderBar {.expand: false.}:
+        showTitleButtons = false
+      
+      ScrolledWindow:
+        PreferencesGroup:
+          title = "Fields"
+          margin = 12
+          
+          for field in fieldWidgets:
+            insert field
 
 proc toAutoForm*[T](app: T): Widget =
   const ignoreFields: seq[string] = @[]
