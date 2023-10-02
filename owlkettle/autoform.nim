da099565a34e11dbe39c45c29396720ccef53f37
Philipp Doerner <philippmdoerner@web.de>
2023-10-02 19:08:59 +0200

    #93 Add an auto mini form generator

--- owlkettle/autoform.nim
+++ owlkettle/autoform.nim
@@ -4,7 +4,6 @@ import ./adw
 import ./widgetutils
 import ./guidsl
 import ./widgetdef
-import ./dataentries
 import ./widgets
 
 macro getField*(someType: untyped, fieldName: static string): untyped =
@@ -14,6 +13,8 @@ proc `%`*(t: tuple): JsonNode =
   result = newJObject()
   for field, value in t.fieldPairs:
     result.add(field, %value)
+
+## General Fields
 
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[SomeFloat]): Widget =
   return gui:
@@ -84,6 +85,104 @@ proc toFormField(state: auto, fieldName: static string, typ: typedesc[DateTime])
         date = state.getField(fieldName)
         proc daySelected(date: DateTime) =
           state.getField(fieldName) = date
+
+## Seq Rows
+proc toSeqField(state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[SomeFloat]): Widget =
+  return gui:
+    NumberEntry():
+      value = state.getField(seqFieldName)[index].getField(fieldName)
+      xAlign = 1.0
+      maxWidth = 8
+      proc changed(value: float) =
+        state.getField(seqFieldName)[index].getField(fieldName) = value
+
+proc toSeqField(state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[SomeInteger]): Widget =
+  return gui:
+    NumberEntry():
+      value = state.getField(seqFieldName)[index].getField(fieldName).float
+      xAlign = 1.0
+      maxWidth = 8
+      proc changed(value: float) =
+        state.getField(seqFieldName)[index].getField(fieldName) = value.int
+
+proc toSeqField(state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[string]): Widget =
+  return gui:
+    Entry():
+      text = state.getField(seqFieldName)[index].getField(fieldName)
+      proc changed(text: string) =
+        state.getField(seqFieldName)[index].getField(fieldName) = text
+
+proc toSeqField(state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[bool]): Widget =
+  return gui:
+    Box():
+      Switch() {.vAlign: AlignCenter, expand: false.}:
+        state = state.getField(seqFieldName)[index].getField(fieldName)
+        proc changed(newVal: bool) =
+          state.getField(seqFieldName)[index].getField(fieldName) = newVal
+
+proc toSeqField[T: enum](state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[T]): Widget =
+  let options: seq[string] = T.items.toSeq().mapIt($it)
+  return gui:
+    DropDown:
+      items = options
+      selected = state.getField(seqFieldName)[index].getField(fieldName).int
+      proc select(enumIndex: int) =
+        state.getField(seqFieldName)[index].getField(fieldName) = enumIndex.T
+        
+proc toSeqField(state: auto, seqFieldName: static string, index: int, fieldName: static string, typ: typedesc[DateTime]): Widget =
+  return gui:
+    Calendar:
+      date = state.getField(seqFieldName)[index].getField(fieldName)
+      proc daySelected(date: DateTime) =
+        state.getField(seqFieldName)[index].getField(fieldName) = date
+
+template myFieldPairs(x: auto) =
+  static: echo $x.type, " ", x is ref object," ",  x is object," ",  x is tuple, " ", x is ref tuple
+  when x is ref object:
+    x[].fieldPairs
+  elif x is object:
+    x.fieldPairs
+  elif x is tuple:
+    x.fieldPairs
+  elif x is ref tuple:
+    x[].fieldPairs
+  else:
+    const t = $x.type
+    {.error: "Not sure what iterator to apply to '" & $t &"'".}
+
+proc createEntryRow[T: object | ref object](state: auto, fieldName: static string, index: int): Widget =
+  var fieldWidgets: seq[AlignedChild[Widget]] = @[]
+  for entryFieldName, entryFieldValue in state.getField(fieldName)[index].fieldPairs:
+    when entryFieldValue.type isnot Option:
+      let fieldWidget: Widget = state.toSeqField(fieldName, index, entryFieldName, entryFieldValue.type)
+      fieldWidgets.add(AlignedChild[Widget](widget: fieldWidget))
+
+  echo "For ", state.getField(fieldName)[index].type, ": ", fieldWidgets.repr
+  
+  return gui:
+    ActionRow:
+      title = fieldName
+      suffixes = fieldWidgets
+
+proc toFormField[T: object | ref object](state: auto, fieldName: static string, typ: typedesc[seq[T]]): Widget =
+  var fieldWidgets: seq[AlignedChild[Widget]] = @[]
+  for index, entry in state.getField(fieldName):
+    let entryRow = createEntryRow[T](state, fieldName, index)
+    fieldWidgets.add(AlignedChild[Widget](widget: entryRow))
+  
+  let adderRow = gui:
+    ListBoxRow:
+      Button:
+        icon = "list-add-symbolic"
+        style = [ButtonFlat]
+        proc clicked() =
+          state.getField(fieldName).add(T())
+  fieldWidgets.add(AlignedChild[Widget](widget: adderRow))
+  
+  return gui:
+    ExpanderRow:
+      title = fieldName
+      rows = fieldWidgets
   
 proc toAutoForm*[T](app: T, ignoreFields: static seq[string]): Widget =
   var fieldWidgets: seq[Widget] = @[]
@@ -106,6 +205,7 @@ proc toAutoForm*[T](app: T, ignoreFields: static seq[string]): Widget =
           
           for field in fieldWidgets:
             insert field
+
 
 proc toAutoForm*[T](app: T): Widget =
   const ignoreFields: seq[string] = @[]
