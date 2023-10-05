f975216062eb5a7ab77033a90b0be4bca67505aa
Philipp Doerner <philippmdoerner@web.de>
2023-10-05 13:17:38 +0200

    #93 Overhaul autoform
    The new implementation enables
    generating an entire form based on either
    `toFormField` for individual entries with custom datatypes
    or `toListFormField` for seq[T] of custom datatypes.

--- owlkettle/autoform.nim
+++ owlkettle/autoform.nim
@@ -1,4 +1,26 @@
-import std/[options, times, macros, strformat, strutils, sequtils, typetraits, enumerate]
+# MIT License
+# 
+# Copyright (c) 2023 Can Joshua Lehmann
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import std/[options, times, macros, strformat, strutils, sequtils, typetraits]
 import ./dataentries
 import ./adw
 import ./widgetutils
@@ -9,6 +31,7 @@ import ./widgets
 macro getField*(someType: untyped, fieldName: static string): untyped =
   nnkDotExpr.newTree(someType, ident(fieldName))
 
+# Default `toFormField` implementations
 proc toFormField[T: SomeNumber](state: auto, fieldName: static string, typ: typedesc[T]): Widget =
   ## Provides a form field in a row for all number times in SomeNumber
   return gui:
@@ -50,15 +73,18 @@ proc toFormField(state: auto, fieldName: static string, typ: typedesc[auto]): Wi
       Label():
         text = fmt"Override `toFormField` for '{$typ.type}'"
         tooltip = fmt"""
-          The type '{$typ.type}' must implement a `toFormField` proc.
+          The type '{$typ.type}' must implement a `toFormField` proc:
           `toFormField(
             state: auto, 
             fieldName: static string, 
             typ: typedesc[{$typ.type}]
           ): Widget`
+          state: The <Widget>State
+          fieldName: The name of the field on `state` for which the current form field is being generated
+          typ: The type for which `toListFormField` shall function.
           
-          This will override this dummy Widget.
-          See other widget example applications for examples.
+          Implementing the proc will override this dummy Widget.
+          See the autoform module for examples.
         """
 
 proc toFormField[T: enum](state: auto, fieldName: static string, typ: typedesc[T]): Widget =
@@ -110,8 +136,6 @@ proc toFormField(state: auto, fieldName: static string, typ: typedesc[DateTime])
           if res.kind == DialogAccept:
             state.getField(fieldName) = DateDialogState(dialogState).date
 
-## Custom DataType toFormFields
-
 proc toFormField(state: auto, fieldName: static string, typ: typedesc[tuple[x,y: int]]): Widget =
   ## Provides a form field in a row for the tuple type of sizeRequest
   return gui:
@@ -131,88 +155,138 @@ proc toFormField(state: auto, fieldName: static string, typ: typedesc[tuple[x,y:
         proc changed(value: float) =
           state.getField(fieldName)[1] = value.int
 
-proc toFormField(state: auto, fieldName: static string, typ: typedesc[seq[ScaleMark]]): Widget =
-  ## Provides a form field in a row for ScaleMark
-  return gui:
-    ExpanderRow:
-      title = fieldName
-      
-      for index, mark in state.getField(fieldName):
-        ActionRow {.addRow.}:
-          title = fieldName & $index
-          
-          NumberEntry {.addSuffix.}:
-            value = mark.value
-            xAlign = 1.0
-            maxWidth = 8
-            proc changed(value: float) =
-              state.getField(fieldName)[index].value = value
-          
-          DropDown {.addSuffix.}:
-            items = ScalePosition.items.toSeq().mapIt($it)
-            selected = mark.position.int
-            proc select(enumIndex: int) =
-              state.getField(fieldName)[index].position = enumIndex.ScalePosition
-              
-          Button {.addSuffix.}:
-            icon = "user-trash-symbolic"
-            proc clicked() =
-              state.getField(fieldName).delete(index)
-      
-      ListBoxRow {.addRow.}:
-        Button:
-          icon = "list-add-symbolic"
-          style = [ButtonFlat]
-          proc clicked() =
-            state.getField(fieldName).add(ScaleMark())
 
-proc toFormField(state: auto, fieldName: static string, typ: typedesc[seq[int]]): Widget =
-  ## Provides a form field in a row for seq[int]
+## Default `toListFormField` implementations
+proc toListFormField[T: SomeNumber](state: auto, fieldName: static string, index: int, typ: typedesc[T]): Widget =
+  ## Provides a single-row form of any number type for displaying an entry in a list
   return gui:
-    ExpanderRow:
-      title = fieldName
+    ActionRow:
+      title = fieldName & $index
       
-      for index, day in state.getField(fieldName):
-        ActionRow {.addRow.}:
-          title = fieldName & $index
-          
-          NumberEntry {.addSuffix.}:
-            value = day.float
-            xAlign = 1.0
-            maxWidth = 8
-            proc changed(value: float) =
-              state.getField(fieldName)[index] = value.int
-      
-      ListBoxRow {.addRow.}:
-        Button:
-          icon = "list-add-symbolic"
-          style = [ButtonFlat]
-          proc clicked() =
-            state.getField(fieldName).add(0)
+      FormulaEntry() {.addSuffix.}:
+        value = state.getField(fieldName)[index].float
+        xAlign = 1.0
+        maxWidth = 8
+        proc changed(value: float) =
+          state.getField(fieldName)[index] = value.T
 
-proc toFormField(state: auto, fieldName: static string, typ: typedesc[seq[string]]): Widget =
-  ## Provides a form field in a row for seq[string]
+proc toListFormField(state: auto, fieldName: static string, index: int, typ: typedesc[string]): Widget =
+  ## Provides a single-row form of string for displaying an entry in a list
+  return gui:
+    ActionRow:
+      title = fieldName
+      Entry() {.addSuffix.}:
+        text = state.getField(fieldName)[index]
+        proc changed(text: string) =
+          state.getField(fieldName)[index] = text
+
+proc toListFormField(state: auto, fieldName: static string, index: int, typ: typedesc[bool]): Widget =
+  ## Provides a single-row form of booleans for displaying an entry in a list
+  return gui:
+    ActionRow:
+      title = fieldName
+      Box() {.addSuffix.}:
+        Switch() {.vAlign: AlignCenter, expand: false.}:
+          state = state.getField(fieldName)[index]
+          proc changed(newVal: bool) =
+            state.getField(fieldName)[index] = newVal
+
+proc toListFormField(state: auto, fieldName: static string, index: int, typ: typedesc[DateTime]): Widget =
+  ## Provides a single-row form of DateTime for displaying an entry in a list
+  return gui:
+    ActionRow:
+      title = fieldName & ": " & $state.getField(fieldName)[index].inZone(local())
+      Button {.addSuffix.}:
+        icon = "x-office-calendar-symbolic"
+        text = "Select"
+        proc clicked() =
+          let (res, dialogState) = state.open(gui(DateDialog()))
+          if res.kind == DialogAccept:
+            state.getField(fieldName)[index] = DateDialogState(dialogState).date
+
+proc toListFormField[T: enum](state: auto, fieldName: static string, index: int, typ: typedesc[T]): Widget =
+  ## Provides a single-row form of an enum for displaying an entry in a list
+  let options: seq[string] = T.items.toSeq().mapIt($it)
+  return gui:
+    ComboRow:
+      title = fieldName
+      items = options
+      selected = ord(state.getField(fieldName)[index])
+      proc select(enumIndex: int) =
+        state.getField(fieldName)[index] = enumIndex.T
+
+proc toListFormField(state: auto, fieldName: static string, index: int, typ: typedesc[auto]): Widget =
+  ## Provides a dummy row widget for displaying an entry in a list of any type without its own `toListFormField`
+  return gui:
+    ActionRow:
+      title = fieldName
+      Label():
+        text = fmt"Override `toListFormField` for '{$typ.type}'"
+        tooltip = fmt"""
+          The type '{$typ.type}' must implement a `toListFormField` proc:
+          `toListFormField(
+            state: auto, 
+            fieldName: static string, 
+            index: int,
+            typ: typedesc[{$typ.type}]
+          ): Widget`
+          state: The <Widget>State
+          fieldName: The name of the field on `state` that contains the seq for which the current form-row is being generated
+          index: The index on the list of entries in `state.<fieldName>`
+          typ: The type for which `toListFormField` shall function.
+          
+          Implementing the proc will override this dummy Widget.
+          See the autoform module for examples.
+        """
+
+proc toListFormField(state: auto, fieldName: static string, index: int, typ: typedesc[ScaleMark]): Widget =
+  ## Provides a single-row form of ScaleMark for displaying an entry in a list
+  let mark: ScaleMark = state.getField(fieldName)[index]
+  return gui:
+    ActionRow:
+      title = fieldName & $index
+      
+      FormulaEntry {.addSuffix.}:
+        value = mark.value
+        xAlign = 1.0
+        maxWidth = 8
+        proc changed(value: float) =
+          state.getField(fieldName)[index].value = value
+      
+      DropDown {.addSuffix.}:
+        items = ScalePosition.items.toSeq().mapIt($it)
+        selected = mark.position.int
+        proc select(enumIndex: int) =
+          state.getField(fieldName)[index].position = enumIndex.ScalePosition
+          
+      Button {.addSuffix.}:
+        icon = "user-trash-symbolic"
+        proc clicked() =
+          state.getField(fieldName).delete(index)
+
+proc toFormField[T](state: auto, fieldName: static string, typ: typedesc[seq[T]]): Widget =
+  ## Provides a form field in a row for all seq type.
+  ## Displays a dummy widget if there is no `toListFormField` implementation for type T.
   return gui:
     ExpanderRow:
       title = fieldName
       
-      for index, val in enumerate(state.getField(fieldName).items):
-        ActionRow {.addRow.}:
-          title = fieldName & $index
-          
-          Entry {.addSuffix.}:
-            text = state.getField(fieldName)[index]
-            proc changed(value: string) =
-              state.getField(fieldName)[index] = value
+      for index, num in state.getField(fieldName):
+        insert(toListFormField(state, fieldName, index, T)){.addRow.}
       
       ListBoxRow {.addRow.}:
         Button:
           icon = "list-add-symbolic"
           style = [ButtonFlat]
           proc clicked() =
-            state.getField(fieldName).add("")
+            state.getField(fieldName).add(default(T))
 
 proc toAutoFormMenu*[T](app: T, sizeRequest: tuple[x,y: int] = (400, 700), ignoreFields: static seq[string]): Widget =
+  ## Provides a form with a form field every field in a given `app` instance. 
+  ## The form is provided as a Popover that can be activated via a Menu-Button
+  ## `ignoreFields` defines a list of field names to ignore for the form generation.
+  ## `sizeRequest` defines the requested size for the popover. 
+  ## Displays a dummy widget if there is no `toFormField` implementation for type T.
   var fieldWidgets: seq[Widget] = @[]
   for name, value in app[].fieldPairs:
     when name notin ["app", "viewed"] and name notin ignoreFields:
