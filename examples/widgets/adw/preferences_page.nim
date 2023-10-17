c95accec8996bc7f71876c0944d4695306ac1b71
Philipp Doerner <philippmdoerner@web.de>
2023-10-17 19:08:16 +0100

    Add bindings and first basic broken example for preferences page

---
+++ examples/widgets/adw/preferences_page.nim
@@ -0,0 +1,67 @@
+# MIT License
+# 
+# Copyright (c) 2022 Can Joshua Lehmann
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import owlkettle, owlkettle/[playground, adw]
+
+viewable App:
+  iconName: string = "weather-clear-symbolic"
+  title: string = "Some Title"
+  name: string = "A name"
+  description: string = "Some Description"
+  useUnderline: bool = false
+  
+  likeLevel: string 
+  reasonForLikingExample: string
+  
+  likeOptions: seq[string] = @["A bit", "A lot", "very much", "Fantastic!",  "I love it!"]
+  
+method view(app: AppState): Widget =
+  result = gui:
+    Window():
+      defaultSize = (800, 600)
+      title = "Preferences Page Example"
+      HeaderBar {.addTitlebar.}:
+        insert(app.toAutoFormMenu(sizeRequest = (400, 250))){.addRight.}
+
+      PreferencesPage():
+        iconName = app.iconName
+        title = app.title
+        name = app.name
+        description = app.description
+        useUnderline = app.useUnderline
+        
+        EntryRow():
+          title = "This example is so cool because:"
+          subtitle = "Truly, just show your thoughts"
+          text = app.reasonForLikingExample
+          
+          proc changed(newText: string) =
+            app.reasonForLikingExample = newText
+        
+        ComboRow():
+          title = "How much do you like this example?"
+          items = app.likeOptions
+          
+          proc select(selectedIndex: int) =
+            app.likeLevel = app.likeOptions[selectedIndex]
+          
+adw.brew(gui(App()))
--- owlkettle/adw.nim
+++ owlkettle/adw.nim
@@ -218,6 +218,54 @@ renderable PreferencesGroup of BaseWidget:
         subtitle = "Subtitle"
         Switch() {.addSuffix.}
 
+renderable PreferencesPage of BaseWidget:
+  preferences: seq[Widget]
+  iconName: string
+  name: string
+  title: string
+  useUnderline: bool
+  description: string
+  
+  hooks:
+    beforeBuild:
+      state.internalWidget = adw_preferences_page_new()
+  
+  hooks preferences:
+    (build, update):
+      state.updateChildren(
+        state.preferences,
+        widget.valPreferences,
+        adw_preferences_page_add,
+        adw_preferences_page_remove
+      )
+  
+  hooks iconName:
+    property:
+      adw_preferences_page_set_icon_name(state.internalWidget, state.iconName.cstring)
+  
+  hooks name:
+    property:
+      adw_preferences_page_set_name(state.internalWidget, state.name.cstring)
+      
+  hooks title:
+    property:
+      adw_preferences_page_set_title(state.internalWidget, state.title.cstring)
+      
+  hooks useUnderline:
+    property:
+      adw_preferences_page_set_use_underline(state.internalWidget, state.useUnderline.cbool)
+      
+  hooks description:
+    property:
+      when AdwVersion >= (1, 4):
+        adw_preferences_page_set_description(state.internalWidget, state.description.cstring)
+      else:
+        raise newException(LibraryError, "Compile for Adwaita version 1.4 or higher with -d:adwMinor=4 to enable the description field for PreferencesPage.")
+  
+  adder add:
+    widget.valPreferences.add(child)
+  
+
 renderable PreferencesRow of ListBoxRow:
   title: string
   
@@ -714,7 +762,7 @@ when AdwVersion >= (1, 2) or defined(owlkettleDocs):
   
   export AboutWindow
 
-export WindowSurface, WindowTitle, Avatar, Clamp, PreferencesGroup, PreferencesRow, ActionRow, ExpanderRow, ComboRow, Flap, SplitButton, StatusPage
+export WindowSurface, WindowTitle, Avatar, Clamp, PreferencesGroup, PreferencesRow, ActionRow, ExpanderRow, ComboRow, Flap, SplitButton, StatusPage, PreferencesPage
 
 proc brew*(widget: Widget,
            icons: openArray[string] = [],
