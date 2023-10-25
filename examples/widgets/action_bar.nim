e60b67b57129e7288d1950a706f13931fd387a92
Philipp Doerner <philippmdoerner@web.de>
2023-10-25 09:14:30 +0100

    Remove unused import

--- examples/widgets/action_bar.nim
+++ examples/widgets/action_bar.nim
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import std/[sequtils]
 import owlkettle, owlkettle/[dataentries, playground, adw]
 
 viewable App:
